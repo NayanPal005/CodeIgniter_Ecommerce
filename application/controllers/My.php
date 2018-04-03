96ebd5f7be1cc8189da60e0f640c1756df89966a
“NayanPal05” <nayancse005@gmail.com>
2018-04-03 18:13:20 +0600

    This is testing about red coloring of file aftter editing project before git

---
+++ application/controllers/My.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class My extends CI_Controller{
+
+    public function index(){
+        echo "This is testing about red color file in CodeIgniter";
+    }
+
+
+}
